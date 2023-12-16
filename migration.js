b5d2ada8c9aaaee35875184da3f8f7146d7898f8
Daniel Echeverri <echeverri121@gmail.com>
2023-12-16 16:26:39 -0500

    Complete step 16

--- migration.js
+++ migration.js
@@ -1,2 +1,18 @@
 const sqlite = require('sqlite3');
 const db = new sqlite3.Database('./database.sqlite');
+
+db.serialize(() => {
+    db.run('DROP TABLE IF EXISTS Artist', error => {
+        if(error)
+        {
+            throw error;
+        }
+    });
+    db.run('CREATE TABLE IF NOT EXISTS `Artist` ( ' +
+    '`id` INTEGER NOT NULL, ' +
+    '`name` TEXT NOT NULL, ' +
+    '`date_of_birth` TEXT NOT NULL, ' +
+    '`biography` TEXT NOT NULL, ' +
+    '`is_currently_employed` INTEGER NOT NULL DEFAULT 1, ' +
+    'PRIMARY KEY(`id`) )');
+});
