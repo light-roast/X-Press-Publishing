af89a469ef9c390292b10fda3d74fe1d5aaea125
Daniel Echeverri <echeverri121@gmail.com>
2023-12-20 11:19:28 -0500

    Completed project

--- src/api/artists.js
+++ src/api/artists.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const artistsRouter = express.Router();
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || '../database.sqlite');
 
 artistsRouter.get('/', (req, res, next) => {
     db.all('SELECT * FROM Artist WHERE is_currently_employed = 1', (err, artists) => {
--- src/api/series.js
+++ src/api/series.js
@@ -2,7 +2,7 @@ const express = require('express');
 const seriesRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const issuesRouter = require('./issues');
-const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || '../database.sqlite');
 
 seriesRouter.get('/', (req, res, next) => {
     db.all('SELECT * FROM Series', (err, series) => {
@@ -94,6 +94,29 @@ seriesRouter.param('seriesId', (req, res, next, id) => {
     } else {
       res.status(400).send('Bad Request. Incomplete data in body req.');
     }
+  });
+
+  seriesRouter.delete('/:seriesId', (req, res, next) => {
+    const issueSql = 'SELECT * FROM Issue WHERE Issue.series_id = $seriesId';
+    const issueValues = {$seriesId: req.params.seriesId};
+    db.get(issueSql, issueValues, (error, issue) => {
+      if (error) {
+        next(error);
+      } else if (issue) {
+        res.sendStatus(400);
+      } else {
+        const deleteSql = 'DELETE FROM Series WHERE Series.id = $seriesId';
+        const deleteValues = {$seriesId: req.params.seriesId};
+  
+        db.run(deleteSql, deleteValues, (error) => {
+          if (error) {
+            next(error);
+          } else {
+            res.sendStatus(204);
+          }
+        });
+      }
+    });
   });
 
   seriesRouter.use('/:seriesId/issues', issuesRouter);
